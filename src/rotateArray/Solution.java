7e61e092150d168956112725d74cd09350e5e407
Kaopong Sangkittiyakorn <kuikaopong.2002@gmail.com>
2024-09-17 01:32:23 +0700

    solve rotate array with memory O(1)

---
+++ src/rotateArray/Solution.java
@@ -0,0 +1,24 @@
+package rotateArray;
+
+public class Solution {
+    final private String url = "https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/";
+
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        int temp;
+        while (start < end) {
+            temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
