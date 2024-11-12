94a486887df4db24a06fea2bb6c2298d125827f6
zkaopong.s <zkaopong.s@motiftech.com>
2024-11-12 11:10:04 +0700

    solved

---
+++ src/linkedListCycle/Solution.java
@@ -0,0 +1,31 @@
+package linkedListCycle;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow) {
+                System.gc();
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
